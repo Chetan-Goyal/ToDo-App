c05b3b0a2d98fecdb7cfd3bbe9cab334ca3d5e90
Chetan-Goyal <harshrock616@gmail.com>
2022-01-11 11:42:54 +0530

    refactors and adds bubble effect in onboarding

--- lib/view/pages/onboarding/onboarding.dart
+++ lib/view/pages/onboarding/onboarding.dart
@@ -4,6 +4,7 @@ import 'package:introduction_screen/introduction_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/constants.dart';
 import 'package:todo_app/view/pages/authentication/signup/signup_screen.dart';
+import 'package:todo_app/view/pages/onboarding/widgets/explore_now_button.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -27,11 +28,13 @@ class OnboardingScreen extends StatelessWidget {
                   width: size.width,
                 ),
                 body: 'Track all of your tasks with our App',
+                decoration: getPageDecoration(),
               ),
               PageViewModel(
                 title: 'Cloud Sync',
                 body: 'Access your tasks on any device',
                 image: SvgPicture.asset("assets/images/onboarding/2-sync.svg"),
+                decoration: getPageDecoration(),
               ),
               PageViewModel(
                 title: 'Easy to use',
@@ -41,22 +44,8 @@ class OnboardingScreen extends StatelessWidget {
                 ),
                 body:
                     'This app is completely ad-free, and helps in keeping track of progress without distractions',
-                footer: ElevatedButton(
-                  onPressed: () {
-                    prefs.data!.setBool('Initialised', true);
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (_) => const SignUpScreen()),
-                    );
-                  },
-                  child: const Text('Explore Now!'),
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: kPrimaryColor,
-                    onPrimary: Colors.white,
-                    onSurface: kPrimaryColor,
-                    primary: kPrimaryColor,
-                  ),
-                ),
+                decoration: getPageDecoration(),
+                footer: ExploreNowButton(prefs: prefs.data!),
               )
             ],
             done: const FittedBox(child: Text('Explore')),
@@ -73,10 +62,34 @@ class OnboardingScreen extends StatelessWidget {
             next: const Text('Next'),
             skip: const Text('Skip'),
             showSkipButton: true,
-            dotsFlex: 5,
+            skipFlex: 0,
+            nextFlex: 0,
+            dotsDecorator: getDotDecoration(),
           ),
         );
       },
+    );
+  }
+
+  PageDecoration getPageDecoration() {
+    return PageDecoration(
+      titleTextStyle:
+          const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      bodyTextStyle: const TextStyle(fontSize: 20),
+      descriptionPadding: const EdgeInsets.all(16).copyWith(bottom: 0),
+      imagePadding: const EdgeInsets.all(24),
+    );
+  }
+
+  DotsDecorator getDotDecoration() {
+    return DotsDecorator(
+      color: kPrimaryColor.withOpacity(0.5),
+      activeColor: kPrimaryColor,
+      size: const Size(10, 10),
+      activeSize: const Size(22, 10),
+      activeShape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24),
+      ),
     );
   }
 }
---
+++ lib/view/pages/onboarding/widgets/explore_now_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_app/constants.dart';
+import 'package:todo_app/view/pages/authentication/signup/signup_screen.dart';
+
+class ExploreNowButton extends StatelessWidget {
+  const ExploreNowButton({
+    Key? key,
+    required this.prefs,
+  }) : super(key: key);
+
+  final SharedPreferences prefs;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        prefs.setBool('Initialised', true);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const SignUpScreen()),
+        );
+      },
+      child: const Text('Explore Now!'),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          side: BorderSide(
+            color: kPrimaryColor.withOpacity(0.4),
+            width: 2.0,
+          ),
+        ),
+        shadowColor: kPrimaryColor,
+        onPrimary: Colors.white,
+        onSurface: kPrimaryColor,
+        primary: kPrimaryColor.withOpacity(0.8),
+      ),
+    );
+  }
+}
