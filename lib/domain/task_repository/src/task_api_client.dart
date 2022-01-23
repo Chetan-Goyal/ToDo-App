c3c30532d72d4e4e2d8c43a72222d9e72c7ada66
Chetan-Goyal <harshrock616@gmail.com>
2022-01-24 03:36:04 +0530

    Removes Print Statement

--- lib/domain/task_repository/src/task_api_client.dart
+++ lib/domain/task_repository/src/task_api_client.dart
@@ -2,8 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:todo_app/config/dio_config.dart';
-import 'package:todo_app/domain/task_repository/src/models/task_model.dart';
-import 'package:todo_app/domain/task_repository/src/task_repository.dart';
+import 'package:todo_app/domain/task_repository/task_repository.dart';
 
 class TaskApiClient implements TaskRepository {
   static String tasksEndpoint = '/v1';
@@ -39,12 +38,11 @@ class TaskApiClient implements TaskRepository {
   }
 
   @override
-  Future getTasks() async {
+  Future<List<TaskModel>> getTasks() async {
     Response response;
 
     String? token = await const FlutterSecureStorage().read(key: 'accessToken');
-    if (token == null) return {'tasks': []};
-    print(token);
+    if (token == null) return [];
     try {
       response = await dioConfig().request(
         "$tasksEndpoint/tasks",
@@ -53,9 +51,9 @@ class TaskApiClient implements TaskRepository {
       );
     } on DioError catch (e) {
       debugPrintStack(stackTrace: e.stackTrace);
-      return {'tasks': []};
+      return [];
     }
-    print(taskModelsFromJson(response.data));
+    // print(taskModelsFromJson(response.data));
     return taskModelsFromJson(response.data);
   }
 
--- lib/view/pages/home/home_screen.dart
+++ lib/view/pages/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/domain/task_repository/task_repository.dart';
 import 'package:todo_app/domain/task_repository/src/task_api_client.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,13 +9,26 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: FutureBuilder(
-            future: TaskApiClient().getTasks(),
-            builder: (context, snapshot) {
-              print(snapshot.data);
-              if (!snapshot.hasData) return const Text("Home Screen");
-              return Text(snapshot.data.toString());
-            }),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder<List<TaskModel>>(
+              future: TaskApiClient().getTasks(),
+              builder: (context, snapshot) {
+                print(snapshot.data);
+                if (!snapshot.hasData) return const CircularProgressIndicator();
+                return ListView.builder(
+                  itemBuilder: (ctx, index) {
+                    return Card(
+                      child: Text(
+                        snapshot.data![index].name,
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    );
+                  },
+                  itemCount: snapshot.data!.length,
+                );
+              }),
+        ),
       ),
     );
   }
