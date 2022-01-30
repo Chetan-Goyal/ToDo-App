90980bfec2ff5d19ede21cedd5a85d98bb748153
Chetan-Goyal <harshrock616@gmail.com>
2022-01-31 03:15:43 +0530

    Replaces Local Environment URL with Live Server URL

--- lib/config/dio_config.dart
+++ lib/config/dio_config.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 Dio dioConfig() {
   return Dio(
     BaseOptions(
-      baseUrl: 'http://localhost:3000/api', // 'https://tasks-manager.azurewebsites.net/api',
+      baseUrl: 'https://tasks-manager.azurewebsites.net/api',
       connectTimeout: 5000,
       receiveTimeout: 3000,
     ),
